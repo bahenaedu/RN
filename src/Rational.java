b0881fa3a90393f2041e15b397e8c935470fb659
bahenaedu <ebahena@coe.edu>
2019-02-02 16:47:25 -0600

    Commenting

--- src/Rational.java
+++ src/Rational.java
@@ -5,30 +5,39 @@ public class Rational {
     private int denominator;
     private static Random rand = new Random();
 
+    /**
+     * The default constructor automatically set the numerator and denominator to 1
+     */
     public Rational()
     {
         numerator = 1;
         denominator = 1;
     }
 
-    public Rational (int x, int y)
+    /**
+     * The constructor ask for two parameters. User choose the numerator and denominator
+     * and check if they are valid
+     * @param num  numerator has to be greater or equal to 0
+     * @param denom denominator has to be greater than 0
+     */
+    public Rational (int num, int denom)
     {
         // the constructor is making sure that either the numerator or denominator fits the conditions
-        if(x >= 0 && y > 0)
+        if(num >= 0 && denom > 0)
         {
-            numerator = x;
-            denominator = y;
+            numerator = num;
+            denominator = denom;
         }
-        else if (x < 0 && y > 0)
+        else if (num < 0 && denom > 0)
         {
-            System.out.println("\nSorry. The numerator cant be " + x +". The program assigned 1 to the numerator.");
+            System.out.println("\nSorry. The numerator cant be " + num +". The program assigned 1 to the numerator.");
             numerator = 1;
-            denominator = y;
+            denominator = denom;
         }
-        else if (x >= 0 && y <= 0)
+        else if (num >= 0 && denom <= 0)
         {
-            System.out.println("\nSorry. The denominator cant be "+ y + ". The program assigned 1 to the denominator.");
-            numerator = x;
+            System.out.println("\nSorry. The denominator cant be "+ denom + ". The program assigned 1 to the denominator.");
+            numerator = num;
             denominator = 1;
         }
         else{
@@ -39,25 +48,58 @@ public class Rational {
         }
     }
 
+    /**
+     * The getter returns the value of numerator
+     * @return int
+     */
     public int getNumerator(){ return numerator; }
+
+    /**
+     * The getter return the value of denominator
+     * @return int
+     */
     public int getDenominator() { return  denominator;}
 
+    /**
+     * The method return a string of the irrational number format n/d
+     * @return String
+     */
     public String toString()
     {
         return "This is your rational number: " + numerator + "/" + denominator + ". Format n/d.";
     }
 
+    /**
+     * The method private is a retrieve function to find the GMC
+     * @param a numerator
+     * @param b denominator
+     * @return GMC value
+     */
     private int gmc (int a, int b) //simplifying the rational number.
     {
         return b == 0 ? a : gmc(b, a % b);
     }
 
+    /**
+     * The method is called in RationalTest class to get the GMC
+     * @param a numerator
+     * @param b denominator
+     * @return value of GMC
+     */
     public int reduce (int a, int b){ // calling from the main
         int  gmc = gmc(a,b);
 
         return gmc;
     }
 
+    /**
+     * The public method is called in RationalTest. it creates a random irrational number
+     * and makes the sum of user's choice and the random irrational number, and then
+     * the result is reduce by using the gmc method and return it.
+     * @param firstNumerator users's numerator
+     * @param firstDenominator user's denominator
+     * @return String
+     */
     public String sum (int firstNumerator, int firstDenominator) // sum of two rational numbers
     {
         int randomNum = rand.nextInt(101);
@@ -78,6 +120,14 @@ public class Rational {
                 (holderNum/holderGmc) + "/" + (holderDeno/holderGmc + " in the simplest form.");
     }
 
+    /**
+     * The public method is called in RationalTest. it creates one random rational number
+     * and multiply with the uses's choice of rational number and
+     * call the gmc to to reduce the irrational as result of the multiplication.
+     * @param firstNum user's numerator
+     * @param firstDeno user's denominator
+     * @return String
+     */
     public String multiply (int firstNum, int firstDeno) //multiplication of two rational numbers
     {
         int randomNum = rand.nextInt(101);
